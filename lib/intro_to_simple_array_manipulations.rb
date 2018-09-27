c52c63c8f25a3e808d3ad1793cfbf665eeaff496
Raquel Randall <raquel.sae.randall@gmail.com>
2018-09-27 18:15:04 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,1 +1,29 @@
 def using_push(array, str)
+  array.push(str)
+end
+
+def using_unshift(array, str)
+  array.unshift(str)
+end
+
+def using_pop(array)
+  array.pop 
+end
+
+def pop_with_args(array)
+  array.pop
+  array.pop
+end
+
+def using_shift(array)
+  array.shift 
+end
+
+def shift_with_args(array)
+  array.shift 
+  array.shift 
+end
+
+def using_concat(arr, array)
+  arr.concat(array)
+end
